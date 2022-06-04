6fc5101cd374ddc4b007211264b77d6d171ad2a3
Tzu Yin <tzyn.wang@gmail.com>
2022-06-04 16:27:28 +0800

    [feat] Componentize as CodeBlock

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,27 +1,6 @@
-import React, { memo, useMemo } from 'react';
-import Code from 'react-syntax-highlighter';
+import React, { memo } from 'react';
 import { kimbieDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import { styled } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Button from '@mui/material/IconButton';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import DoneIcon from '@mui/icons-material/Done';
-import CloseIcon from '@mui/icons-material/Close';
-import useCopy from '@Hooks/useCopy';
-
-const CodeContainer = styled('div')(() => ({
-  position: 'relative',
-  '&:hover .IconButton': {
-    opacity: 1
-  }
-}));
-const IconButton = styled(Button)(({ theme }) => ({
-  position: 'absolute',
-  top: '1rem',
-  right: '1rem',
-  opacity: 0,
-  transition: `opacity ${theme.transitions.duration.shortest}ms ${theme.transitions.easing.easeIn}`
-}));
+import CodeBlock from '@Components/Common/CodeBlock';
 
 function App(): React.ReactElement {
   /* States */
@@ -66,37 +45,10 @@ function createElement({ node, style, useInlineStyles, key }) {
     return <TagName key={key} {...props}>{children}</TagName>;
   }
 }`;
-  const [copyDone, handleCopy] = useCopy();
-  const copyIcon = useMemo(() => {
-    const iconStyle = {
-      color: kimbieDark['hljs-comment']?.color,
-      fontSize: '1rem'
-    };
-    if (copyDone) {
-      return <DoneIcon sx={iconStyle} />;
-    }
-    if (copyDone === false) {
-      return <CloseIcon sx={iconStyle} />;
-    }
-    return <ContentCopyIcon sx={iconStyle} />;
-  }, [copyDone]);
 
   /* Main */
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <CodeContainer>
-        <Code language="javascript" style={kimbieDark} wrapLongLines>
-          {codeString}
-        </Code>
-        <IconButton
-          onClick={() => handleCopy(codeString)}
-          className="IconButton"
-        >
-          {copyIcon}
-        </IconButton>
-      </CodeContainer>
-    </React.Fragment>
+    <CodeBlock code={codeString} style={kimbieDark} language="javascript" wrapLongLines />
   );
 }
 
