504e67470be35758a9d53825dcdddffadb762ba2
Tzu Yin <tzyn.wang@gmail.com>
2022-06-05 19:34:05 +0800

    [feat] Update demo code content

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,11 +4,88 @@ import CodeBlock from '@Components/Common/CodeBlock';
 
 function App(): React.ReactElement {
   /* States */
-  const codeString = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim nemo neque libero, repellat a quisquam deleniti ex veritatis natus.';
+  const codeString = 
+`import React, { memo, useMemo } from 'react';
+import Code from 'react-syntax-highlighter';
+import type { SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/IconButton';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
+import useCopy from '@Hooks/useCopy';
+
+type SyntaxHighlighterPropsWithoutChildren = Omit<
+  SyntaxHighlighterProps,
+  'children'
+>;
+
+interface CodeBlockProps extends SyntaxHighlighterPropsWithoutChildren {
+  code: string;
+  style?: { [key: string]: React.CSSProperties };
+}
+
+const CodeContainer = styled('div')(() => ({
+  position: 'relative',
+  '&:hover .IconButton': {
+    opacity: 1
+  }
+}));
+const IconButton = styled(Button)(({ theme }) => ({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  opacity: 0,
+  transition: \`opacity \${theme.transitions.duration.shortest}ms \${theme.transitions.easing.easeIn}\`
+}));
+
+function CodeBlock(props: CodeBlockProps): React.ReactElement {
+  /* States */
+  const { code, style, ...rest } = props;
+  const [copyDone, handleCopy] = useCopy();
+  const copyIcon = useMemo(() => {
+    const iconStyle = {
+      color: style ? style['hljs-comment']?.color : undefined,
+      fontSize: '1rem'
+    };
+    if (copyDone) {
+      return <DoneIcon sx={iconStyle} />;
+    }
+    if (copyDone === false) {
+      return <CloseIcon sx={iconStyle} />;
+    }
+    return <ContentCopyIcon sx={iconStyle} />;
+  }, [copyDone, style]);
+
+  /* Functions */
+  const handleIconButtonClick = (): void => {
+    handleCopy(code);
+  };
 
   /* Main */
   return (
-    <CodeBlock code={codeString} style={kimbieDark} language="javascript" wrapLongLines />
+    <CodeContainer className="CodeContainer">
+      <Code style={style} {...rest}>
+        {code}
+      </Code>
+      <IconButton
+        onClick={handleIconButtonClick}
+        className="IconButton"
+        disableFocusRipple
+        disableRipple
+      >
+        {copyIcon}
+      </IconButton>
+    </CodeContainer>
+  );
+}
+
+export default memo(CodeBlock);
+`
+
+  /* Main */
+  return (
+    <CodeBlock code={codeString} style={kimbieDark} language="typescript" wrapLongLines />
   );
 }
 
